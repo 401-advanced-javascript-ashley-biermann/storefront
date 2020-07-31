18f50979b9ef65649e9baa001206ba2efe6e03c1
ashleybiermann <ashleybiermann@gmail.com>
2020-07-30 22:44:08 -0700

    adds basic browserroute and routes, not yet actually sending to the linked page, just changing the url, created checkout cart holder

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './components/header/header.js';
 import Footer from './components/footer/footer.js';
 import Categories from './components/storefront/categories.js';
 import Products from './components/storefront/products.js';
 import SimpleCart from './components/cart/simplecart.js';
+import CheckoutCart from './components/cart/checkout.js';
 
 // Setting up the global state
 import { Provider } from 'react-redux';
@@ -18,9 +19,21 @@ export default class App extends React.Component {
       <>
         <Provider store={store}>
           <Header />
-          <Categories />
-          <SimpleCart />
-          <Products />
+          <BrowserRouter>
+
+            <Route exact path="/">
+              <Categories />
+              <SimpleCart />
+              <Products />
+            </Route>
+
+
+            <Route exact path="/cart">
+              <CheckoutCart />
+            </Route>
+
+
+          </BrowserRouter>
           <Footer />
         </Provider>
       </>
